3db069b419ae622cefb353bf3e88e8e629322193
javvadi-sai-ram <54625079+javvadi-sai-ram@users.noreply.github.com>
2023-02-25 11:15:50 -0600

    Update hp_4.py

--- src/hp_4.py
+++ src/hp_4.py
@@ -6,25 +6,32 @@ from collections import defaultdict
 
 
 def reformat_dates(old_dates):
-    """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
-    element to a format dd mmm yyyy--01 Jan 2001."""
-    pass
+    list_of_date=[]
+
+    for each in old_dates:
+        list_of_date.append(datetime.strptime(each,"%Y-%m-%d").strftime("%d,%b,%Y"))
+    return list_of_date
 
 
 def date_range(start, n):
-    """For input date string `start`, with format 'yyyy-mm-dd', returns
-    a list of of `n` datetime objects starting at `start` where each
-    element in the list is one day after the previous."""
-    pass
-
+    if not isinstance(start,str):
+        raise TypeError
+    elif not isinstance(n,int):
+        raise TypeError
+    else:
+        list_of_date=[]
+        for index in range(n):
+            list_of_date.append(datetime.strptime(start,"%Y-%m-%d")  + timedelta(days=index))
+            return list_of_date
 
 def add_date_range(values, start_date):
-    """Adds a daily date range to the list `values` beginning with
-    `start_date`.  The date, value pairs are returned as tuples
-    in the returned list."""
-    pass
+    list_of_date=[]
 
+    for index,value in enumerate(values):
+        list_of_date.append(datetime.strptime(start_date,"%Y-%m-%d")  + timedelta(days=i))
+        return list(zip(list_of_date,values))
 
+        
 def fees_report(infile, outfile):
     """Calculates late fees per patron id and writes a summary report to
     outfile."""
